17b966fdbd7858cc14afe8677dc95264235fd509
blaiseirakoze <blaiseirakoze50@gmail.com>
2021-09-07 19:09:21 +0200

    ch-setup-environment

---
+++ src/test/java/com/atn/mnomanager/MnoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atn.mnomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MnoManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
